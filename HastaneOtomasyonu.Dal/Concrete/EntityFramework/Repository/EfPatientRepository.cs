ffe2995bd37ea876a31cc6cbd676a6d5f095c61b
ufukgulec <oug505@gmail.com>
2021-04-03 14:24:41 +0300

    EfPatientRepository oluşturuldu.

---
+++ HastaneOtomasyonu.Dal/Concrete/EntityFramework/Repository/EfPatientRepository.cs
@@ -0,0 +1,32 @@
+﻿using HastaneOtomasyonu.Dal.Abstract;
+using HastaneOtomasyonu.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneOtomasyonu.Dal.Concrete.EntityFramework.Repository
+{
+    public class EfPatientRepository : EfGenericRepository<Patient>, IPatientRepository
+    {
+        public EfPatientRepository() : base()
+        {
+
+        }
+        public List<Patient> DateOfPatients(DateTime dateTime)
+        {
+            return _context.Patient.Where(x => x.dateofregistration == dateTime).ToList();
+        }
+
+        public List<Patient> FemalePatients()
+        {
+            return _context.Patient.Where(x => x.gender.Contains("k")).ToList();
+        }
+
+        public List<Patient> MalePatients()
+        {
+            return _context.Patient.Where(x => x.gender.Contains("e")).ToList();
+        }
+    }
+}
