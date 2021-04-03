2c600e3e7180610cc0616d9d569c030291a77886
ufukgulec <oug505@gmail.com>
2021-04-03 13:03:51 +0300

    deneme

--- HastaneOtomasyonu.Business/GenericManager.cs
+++ HastaneOtomasyonu.Business/GenericManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HastaneOtomasyonu.Dal.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace HastaneOtomasyonu.Business
 {
     public class GenericManager
     {
+        
     }
 }
