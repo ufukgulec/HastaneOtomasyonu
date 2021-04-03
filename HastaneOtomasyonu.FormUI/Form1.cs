0581d393714ec14f2960798f6c3ffa71480c9a56
ufukgulec <oug505@gmail.com>
2021-04-03 13:31:11 +0300

    İlk çalıştırma

--- HastaneOtomasyonu.FormUI/Form1.cs
+++ HastaneOtomasyonu.FormUI/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HastaneOtomasyonu.Business;
+using HastaneOtomasyonu.Dal.Abstract;
+using HastaneOtomasyonu.Dal.Concrete.EntityFramework.Repository;
+using HastaneOtomasyonu.Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +16,7 @@ namespace HastaneOtomasyonu.FormUI
 {
     public partial class Form1 : Form
     {
+        GenericManager<Patient> patientService = new GenericManager<Patient>(new EfGenericRepository<Patient>());
         public Form1()
         {
             InitializeComponent();
