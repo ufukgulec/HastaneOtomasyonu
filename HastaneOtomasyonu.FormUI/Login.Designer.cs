4b4eede95480689bd213838436cf57ac4e3942c6
ufukgulec <oug505@gmail.com>
2021-04-09 20:03:14 +0300

    Login Tasarım

--- HastaneOtomasyonu.FormUI/Login.Designer.cs
+++ HastaneOtomasyonu.FormUI/Login.Designer.cs
@@ -35,9 +35,9 @@ namespace HastaneOtomasyonu.FormUI
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tcNo = new System.Windows.Forms.TextBox();
+            this.parola = new System.Windows.Forms.TextBox();
+            this.giris = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.panel1.SuspendLayout();
@@ -67,9 +67,9 @@ namespace HastaneOtomasyonu.FormUI
             // 
             this.panel3.Controls.Add(this.linkLabel1);
             this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Controls.Add(this.textBox2);
-            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.giris);
+            this.panel3.Controls.Add(this.parola);
+            this.panel3.Controls.Add(this.tcNo);
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(200, 0);
@@ -97,36 +97,38 @@ namespace HastaneOtomasyonu.FormUI
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // textBox1
+            // tcNo
             // 
-            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(6, 199);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(372, 29);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Tc Kimlik No";
+            this.tcNo.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tcNo.Location = new System.Drawing.Point(6, 199);
+            this.tcNo.Name = "tcNo";
+            this.tcNo.Size = new System.Drawing.Size(372, 29);
+            this.tcNo.TabIndex = 1;
+            this.tcNo.Enter += new System.EventHandler(this.tcNo_Enter);
+            this.tcNo.Leave += new System.EventHandler(this.tcNo_Leave);
             // 
-            // textBox2
+            // parola
             // 
-            this.textBox2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox2.Location = new System.Drawing.Point(6, 234);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(372, 29);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "Parola";
+            this.parola.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.parola.Location = new System.Drawing.Point(6, 234);
+            this.parola.Name = "parola";
+            this.parola.Size = new System.Drawing.Size(372, 29);
+            this.parola.TabIndex = 2;
+            this.parola.Enter += new System.EventHandler(this.parola_Enter);
+            this.parola.Leave += new System.EventHandler(this.parola_Leave);
             // 
-            // button1
+            // giris
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(177)))), ((int)(((byte)(172)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(6, 269);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(372, 33);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Giriş";
-            this.button1.UseVisualStyleBackColor = false;
+            this.giris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(177)))), ((int)(((byte)(172)))));
+            this.giris.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.giris.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.giris.ForeColor = System.Drawing.Color.White;
+            this.giris.Location = new System.Drawing.Point(6, 269);
+            this.giris.Name = "giris";
+            this.giris.Size = new System.Drawing.Size(372, 33);
+            this.giris.TabIndex = 3;
+            this.giris.Text = "Giriş";
+            this.giris.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -169,6 +171,7 @@ namespace HastaneOtomasyonu.FormUI
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -186,9 +189,9 @@ namespace HastaneOtomasyonu.FormUI
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button giris;
+        private System.Windows.Forms.TextBox parola;
+        private System.Windows.Forms.TextBox tcNo;
         private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- HastaneOtomasyonu.FormUI/Login.cs
+++ HastaneOtomasyonu.FormUI/Login.cs
@@ -16,5 +16,53 @@ namespace HastaneOtomasyonu.FormUI
         {
             InitializeComponent();
         }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            tcNo.Text = "Tc Kimlik No";
+            tcNo.ForeColor = Color.Gray;
+            parola.Text = "Parola Giriniz";
+            parola.ForeColor = Color.Gray;
+            this.ActiveControl = giris;
+
+        }
+        private void tcNo_Enter(object sender, EventArgs e)
+        {
+            if (tcNo.Text == "Tc Kimlik No")
+            {
+                tcNo.Text = "";
+                tcNo.ForeColor = Color.Black;
+            }
+        }
+
+        private void tcNo_Leave(object sender, EventArgs e)
+        {
+            if (tcNo.Text == "")
+            {
+                tcNo.Text = "Tc Kimlik No";
+                tcNo.ForeColor = Color.Gray;
+            }
+        }
+
+
+
+        private void parola_Enter(object sender, EventArgs e)
+        {
+            if (parola.Text == "Parola Giriniz")
+            {
+                parola.Text = "";
+                parola.ForeColor = Color.Black;
+                parola.PasswordChar = '*';
+            }
+        }
+
+        private void parola_Leave(object sender, EventArgs e)
+        {
+            if (parola.Text == "")
+            {
+                parola.Text = "Parola Giriniz";
+                parola.ForeColor = Color.Gray;
+                parola.PasswordChar = '\0';
+            }
+        }
     }
 }
